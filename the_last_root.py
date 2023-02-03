aefb7f0438f8935710c8ec4f8ab6358408f1b6e0
LucasLoustalot <lucas.loustalot@epitech.eu>
2023-02-03 21:01:30 +0100

    Initiaal Commit

---
+++ the_last_root.py
@@ -0,0 +1,15 @@
+##
+## EPITECH PROJECT, 2023
+## the_last_root
+## File description:
+## the_last_root
+##
+
+import pygame
+
+def main():
+    return (0)
+
+if __name__ == '__main__':
+    main()
+
