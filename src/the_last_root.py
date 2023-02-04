dd3265f0a8d6b3c461409fbb85c38e701244e43e
LucasLoustalot <lucas.loustalot@epitech.eu>
2023-02-04 16:10:43 +0100

    Testing User Interface

--- src/the_last_root.py
+++ src/the_last_root.py
@@ -9,12 +9,19 @@ import pygame
 from game import *
 from player import *
 from ant import *
+from user_interface import *
 CONST_POS = (800, 400)
+
+def ditnon():
+    print("NON !")
 
 def main():
     pgm = Game((1600,800),60,"The Last Root","../assets/photobg.jpg")
-    pgm.add_object(Player(["../assets/flower1.png","../assets/photobg.jpg"],CONST_POS,0,(400,400),pgm),1)
+    
+    pgm.add_object(Player(["../assets/flower1.png","../assets/photobg.jpg"],CONST_POS,0,(400,400),pgm),0)
     ant(pgm, CONST_POS)
+    pgm.add_object(Upgrade_Button(["../assets/playbtnplaceholder.png"],
+    (200,200),0,(500,100),pgm,ditnon,"Dit non"),2)
     while (1):
         pgm.update()
     return (0)
