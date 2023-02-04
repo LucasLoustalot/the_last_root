0befe97b90fbda2e28f94cc1f038d09386a73903
RaphxelS <raphael.scandella@epitech.eu>
2023-02-04 11:14:49 +0100

    random

--- src/ant.py
+++ src/ant.py
@@ -7,6 +7,8 @@
 
 from game import *
 import pygame
+import random
+import math
 
 class Ant(Game_Object):
     """Ant Specific Class"""
@@ -28,12 +30,20 @@ class Ant(Game_Object):
     def event_clicked(self):
         return
 
-def ant(game: Game):
+def ant(game: Game, pos: tuple):
     j = 1
-    antl = []
+    antl: Ant = []
     for i in range(0, j * 5):
+        random1 = random.choice([0, 1500])
+        random2 = random.randint(0, 800)
         antl.append(Ant(["../assets/ant.png","../assets/ant.png"],
-        (20 + ((i + j) * 200), 20),0, (200, 200), game))
+        (random1, random2), angle_between_players(pos, (random1, random2)), (150, 150), game))
     for i in range(0, j * 5):
         game.add_object(antl[i], 1)
 
+def angle_between_players(pos, pos2):
+    x1, y1 = pos
+    x2, y2 = pos2
+    x_diff = x2 - x1
+    y_diff = y2 - y1
+    return math.degrees(math.atan2(y_diff, x_diff))
--- src/the_last_root.py
+++ src/the_last_root.py
@@ -9,11 +9,12 @@ import pygame
 from game import *
 from player import *
 from ant import *
+CONST_POS = (300, 300)
 
 def main():
     pgm = Game((1600,800),60,"The Last Root","../assets/photobg.jpg")
-    pgm.add_object(Player(["../assets/flower1.png","../assets/photobg.jpg"],(300,300),0,(400,400),pgm),1)
-    ant(pgm)
+    pgm.add_object(Player(["../assets/flower1.png","../assets/photobg.jpg"],CONST_POS,0,(400,400),pgm),1)
+    ant(pgm, CONST_POS)
     while (1):
         pgm.update()
     return (0)
