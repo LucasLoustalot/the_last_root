f2c9dcbe6e29d28995e2c9a9dfb5821eec815bb8
LucasLoustalot <lucas.loustalot@epitech.eu>
2023-02-04 09:55:52 +0100

    Update path

--- src/the_last_root.py
+++ src/the_last_root.py
@@ -10,8 +10,8 @@ from game import *
 from player import *
 
 def main():
-    pgm = Game((1600,800),60,"The Last Root","photobg.jpg")
-    pgm.add_object(Player(["flower1.png","photobg.jpg"],(300,300),0,(400,400),pgm),1)
+    pgm = Game((1600,800),60,"The Last Root","../assets/photobg.jpg")
+    pgm.add_object(Player(["../assets/flower1.png","../assets/photobg.jpg"],(300,300),0,(400,400),pgm),1)
     while (1):
         pgm.update()
     return (0)
